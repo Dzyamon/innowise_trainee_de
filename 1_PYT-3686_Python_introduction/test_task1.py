f00bad9bad81c7a55819b3e0ac277f75622fb5b3
Dzyamon <dmitri.olehnovich@gmail.com>
2024-02-05 17:45:22 +0100

    fix test and cleared gitignore

--- 1_PYT-3686_Python_introduction/test_task1.py
+++ 1_PYT-3686_Python_introduction/test_task1.py
@@ -1,31 +1,22 @@
-<<<<<<< HEAD
-import task1_old
-=======
-import task1
->>>>>>> dev
+# import task1_old
+# import task1
+import main
 import pytest
 import psycopg2
+from dotenv import load_dotenv
+import os
 
 
 # test check if schema "task1" exists
-<<<<<<< HEAD
 # run with pytest -s (due to input)
 @pytest.fixture(scope="module")
 def db_connection():
-    conn = psycopg2.connect(host=task1_old.host,
-                            port=task1_old.port,
-                            dbname=task1_old.database,
-                            user=task1_old.user_name,
-                            password=task1_old.user_password)
-=======
-@pytest.fixture(scope="module")
-def db_connection():
-    conn = psycopg2.connect(host=task1.host,
-                            port=task1.port,
-                            dbname=task1.database,
-                            user=task1.user_name,
-                            password=task1.user_password)
->>>>>>> dev
+    load_dotenv()
+    conn = psycopg2.connect(host=os.environ.get("HOST"),
+                            port=os.environ.get("PORT"),
+                            dbname=os.environ.get('DATABASE'),
+                            user=os.environ.get("USER_NAME"),
+                            password=os.environ.get("USER_PASSWORD"))
     yield conn
 
 
